e393ed3a062c3187eca7584c37c6016dc4e7f610
RodiTolga <rodizayif@gmail.com>
2023-08-12 16:34:01 +0100

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,43 @@
+
+
+import random
+
+from hangman_words import word_list
+from hangman_art import logo
+print(logo)
+lives = 6
+
+attempts = []
+chosen_word = random.choice(word_list)                #Choosing random word from imported word_list
+display=[] 
+wordlength = len(chosen_word)
+for _ in range(wordlength):  
+  display += "_"                                      #Uses len function to count no. of letters in word and replaces it with a _
+print(display)
+
+end_of_game = False
+
+while not end_of_game:                  
+
+  guess = input("Guess a letter: ").lower()
+  if guess in display:
+    print(f"You've already gussed {guess}")
+  for position in range(wordlength):
+    letter = chosen_word[position]
+    if letter == guess:                 
+      display[position] = letter                      #If chosen letter is in the word, then replace the _ with the letter
+  if guess not in chosen_word:
+    attempts.append(guess)
+    print(attempts)
+    print(f"You guessed {guess}, that's not in the word. You lose a life ")
+    lives -= 1                                        #If chosen letter isnt in the word, then lose a life
+    if lives == 0:
+      end_of_game = True                              #After 6 incorrect choices, end_of_game changes to true and game ends
+      print("You lose")
+  print(display)
+
+  if "_" not in display:                              #When there's no more _ in the display, then end_of_game becomes True and you win. 
+    end_of_game = True
+    print("You win!")
+  from hangman_art import stages
+  print(stages[lives])                                #With every incorrect answer, another body part is added to the hangman. 
