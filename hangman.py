a714a6d203ddaf0343677381689fcaedc284eccc
RodiTolga <rodizayif@gmail.com>
2023-09-07 02:07:24 +0100

    added command that reveals word during loss

--- hangman.py
+++ hangman.py
@@ -20,20 +20,21 @@ end_of_game = False
 while not end_of_game:                  
 
   guess = input("Guess a letter: ").lower()
-  if guess in display:
+  if guess in display:                                #If the letter gussed has already been guessed and was correct, then don't punish
     print(f"You've already gussed {guess}")
   for position in range(wordlength):
     letter = chosen_word[position]
     if letter == guess:                 
       display[position] = letter                      #If chosen letter is in the word, then replace the _ with the letter
   if guess not in chosen_word:
-    attempts.append(guess)
+    attempts.append(guess)                            #Adds the incorrect choices to attempts array to show the user what letters have been used already
     print(attempts)
     print(f"You guessed {guess}, that's not in the word. You lose a life ")
     lives -= 1                                        #If chosen letter isnt in the word, then lose a life
     if lives == 0:
       end_of_game = True                              #After 6 incorrect choices, end_of_game changes to true and game ends
       print("You lose")
+      print(f"The word was {chosen_word}")
   print(display)
 
   if "_" not in display:                              #When there's no more _ in the display, then end_of_game becomes True and you win. 
